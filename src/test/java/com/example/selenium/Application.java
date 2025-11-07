f7b74e83f0f71a11fc6e9eb300fc8cc4a68e965c
guilhermeklein-gp <guilherme.klein@globalpay.com>
2025-11-06 21:37:16 -0300

    Adding 2 different environments

--- src/test/java/com/example/selenium/Application.java
+++ src/test/java/com/example/selenium/Application.java
@@ -1,12 +1,32 @@
 package com.example.selenium;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    @Value("${selenium.browser}")
+    private String browser;
 
+    @Value("${target.application.baseUrl}")
+    private String baseUrl;
+
+    @Value("${spring.config.name:application}")
+    private String configFile;
+
+    @PostConstruct
+    public void printEnvironmentDetails() {
+        System.out.println("Environment loaded from: " + configFile + ".properties");
+        System.out.println("Browser: " + browser);
+        System.out.println("Base URL: " + baseUrl);
+        System.out.println("--------");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
-
